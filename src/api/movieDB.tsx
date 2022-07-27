40dc4c8c0c0262effb2ef996894cc3f8377a4708
Sebastián R. Rojas <100496049+defSebas@users.noreply.github.com>
2022-07-27 13:48:14 -0600

    Update movieDB.tsx

--- src/api/movieDB.tsx
+++ src/api/movieDB.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const movieDB = axios.create({
     baseURL: 'https://api.themoviedb.org/3/movie',
     params: {
-        api_key: 'd95fa96cc24f5711805bb8119d520d2d',
+        api_key: 'HERE GOES YOUR API KEY',
         language: 'es-ES',
     },
 });
